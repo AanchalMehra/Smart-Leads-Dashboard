838ba7f97926acbab0cd1994530447ba795d9576
AanchalMehra <aanchalmehra15@gmail.com>
2026-05-18 23:34:01 +0530

    add Lead model

---
+++ src/models/Lead.ts
@@ -0,0 +1,47 @@
+import mongoose, { Schema } from "mongoose";
+
+export interface leadBody {
+  name: string;
+  email: string;
+  status: "New"|"Contacted"|"Qualified"|"Lost";
+  source: "Website"|"Instagram"|"Referral";
+  createdBy: mongoose.Types.ObjectId;
+}
+
+const leadSchema = new Schema<leadBody>(
+  {
+    name:{
+      type:String,
+      required:true,
+      trim:true,
+    },
+
+    email:{
+      type:String,
+      required:true,
+      lowercase:true,
+    },
+
+    status: {
+      type:String,
+      enum:["New","Contacted","Qualified","Lost"],
+      default:"New",
+    },
+
+    source: {
+      type:String,
+      enum:["Website", "Instagram", "Referral"],
+      required:true,
+    },
+
+    createdBy: {
+      type:Schema.Types.ObjectId,
+      ref:"User",
+      required:true,
+    }
+
+  },{timestamps:true})
+
+const Lead=mongoose.model<leadBody>("Lead", leadSchema);
+
+export default Lead;
